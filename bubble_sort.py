3ee22c01f0c55e65d0bf6207059913c25f4c752a
tsUki-Co-rO <coro.chaos@gmail.com>
2018-01-23 13:38:31 +0800

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#冒泡法
+import random
+
+def randomlist():
+	random_list = []
+	for i in range(10):
+		random_list.append(random.randint(1, 50))
+	return random_list
+
+x = randomlist()
+
+def bubble_sort(a):
+	i = 0
+	j = 0
+	for j in xrange(0,len(a)-j):
+		for i in xrange(0,len(a)-j-1):
+			if a[i] > a[i+1]:
+				tmp = a[i+1]
+				a[i+1] = a[i]
+				a[i] = tmp
+	return a
+print x
+print bubble_sort(x)
